985fa9e2d4de95efb05ff8ce48da7552b7f6e09c
Rayhan Kabir <rayhan.kabir29@gmail.com>
2024-03-10 12:50:07 +0600

    pricing plan

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./*.html"],
   theme: {
     extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+      },
       colors: {
         "bg-color": "#171b1a",
         "left-color": "#F5BD4D",
